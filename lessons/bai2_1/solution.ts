17b7a6e400b4441e8c475a1199910238640fb064
Duong Tan Phat <phat.duongtan.cit22@eiu.edu.vn>
2026-06-12 09:22:48 +0700

    bài 2_1 Mô phỏng Smart Contract

--- lessons/bai2_1/solution.ts
+++ lessons/bai2_1/solution.ts
@@ -2,14 +2,14 @@ export class SmartContract {
     private message: string;
 
     constructor(initialMessage: string) {
-        // TODO: initialize the message with initialMessage
+        this.message = initialMessage;
     }
 
     public updateMessage(newMsg: string): void {
-        // TODO: update the message with newMsg
+        this.message = newMsg;
     }
 
     public getMessage(): string {
-        // TODO: return the current message
+        return this.message;
     }
 }
